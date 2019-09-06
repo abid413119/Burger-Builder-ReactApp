56206dfb0a15eb28fb4a14a7ff8d89debb154637
Abid Al Amin <abidastronaut@gmail.com>
2019-09-06 15:20:20 +0600

    Updates

---
+++ precache-manifest.ec475b46a868bf7d5b141f57b3eb7781.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "767fc6cffdad145a291a7dcae513d634",
+    "url": "/Burger-Builder-ReactApp/index.html"
+  },
+  {
+    "revision": "96f5f11f53d2cf020a47",
+    "url": "/Burger-Builder-ReactApp/static/css/main.6d4351d2.chunk.css"
+  },
+  {
+    "revision": "b26129f75834355090cc",
+    "url": "/Burger-Builder-ReactApp/static/js/2.5e3b1a22.chunk.js"
+  },
+  {
+    "revision": "96f5f11f53d2cf020a47",
+    "url": "/Burger-Builder-ReactApp/static/js/main.abcceb39.chunk.js"
+  },
+  {
+    "revision": "681b2b6ca36db37882e5",
+    "url": "/Burger-Builder-ReactApp/static/js/runtime~main.9638f02f.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/Burger-Builder-ReactApp/static/media/burger-logo.b8503d26.png"
+  }
+]);
